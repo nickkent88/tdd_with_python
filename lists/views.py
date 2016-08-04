32d0e5d093eabcad4d43154848b501bc77e47e4d
Nick Kent <nickkent88@gmail.com>
2016-08-04 09:37:36 -0600

    Fix tabbing

--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
 from lists.models import Item
 
-# Create your views here.
 def home_page(request):
-  if request.method == 'POST':
-    Item.objects.create(text=request.POST['item_text'])
-    return redirect('/')
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-  items = Item.objects.all()
-  return render(request, 'home.html', {'items': items})
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
